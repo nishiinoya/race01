65d09781a6cc857fee3a316ed19324d845049d1a
nishiinoya <omalanchuk.wrk@gmail.com>
2024-02-18 11:35:32 +0100

    final final 100%

--- src/decoder.c
+++ src/decoder.c
@@ -9,20 +9,31 @@ int invalid_op(const char *s) {
         if (s[i] == '*' || s[i] == '+' || s[i] == '/' || s[i] == '-' || s[i] == '?')
             number_of_ok++;
         else
-            return 1; // return 1 because invalid (unpredicted character)
+            return 1; // invalid (unpredicted character)
 
         if (number_of_ok > 1)
-            return 1; // return 1 because invalid (too many operands)
+            return 1; // invalid (too many operands)
     }
     if (number_of_ok != 1)
-        return 1; // return 1 because invalid (no operands)
+        return 1; // invalid (no operands)
     else
         return 0; // if reached here we have only ' ' and 1 "ok" operand
 }
 int invalid_n(const char *s) {
+    int start = 0;
     for (int i = 0; s[i] != '\0'; i++)
-        if (!((s[i] - '0' >= 0 && s[i] - '0' < 10) || s[i] == ' ' || s[i] == '?'))
-            return 1;
+        if (!((s[i] - '0' >= 0 && s[i] - '0' < 10) || s[i] == ' ' || s[i] == '?')) {
+            if (s[i] == '-') {
+                start++;
+                if (start > 1)
+                    return 1;
+            } else {
+                return 1;
+            }
+        } else {
+            if (!start)
+                start++;
+        }
 
     return 0;
 }
@@ -43,24 +54,30 @@ char *format(char *x) {
     y[j] = '\0';
     return y;
 }
-void check(char *operation, char * op1, char *op2, char *res) {
+void check(char *operation, char *op1, char *op2, char *res) {
     int o1 = 0;
     for (int i = 0; op1[i] != '\0'; i++) {
         o1 *= 10;
-        o1 += (op1[i] - '0');
+        o1 += (op1[i] != '-' ? op1[i] - '0' : 0);
     }
+    if (op1[0] == '-')
+        o1 = -o1;
     int o2 = 0;
     for (int i = 0; op2[i] != '\0'; i++) {
         o2 *= 10;
-        o2 += (op2[i] - '0');
+        o2 += (op2[i] != '-' ? op2[i] - '0' : 0);
     }
+    if (op2[0] == '-')
+        o2 = -o2;
     int r = 0;
     for (int i = 0; res[i] != '\0'; i++) {
         r *= 10;
-        r += (res[i] - '0');
+        r += (res[i] != '-' ? res[i] - '0' : 0);
     }
+    if (res[0] == '-')
+        r = -r;
 
-    if (*operation== '+') {
+    if (*operation == '+') {
         if (o1 + o2 == r) {
             mx_printint(o1);
             mx_printstr(" + ");
@@ -69,7 +86,7 @@ void check(char *operation, char * op1, char *op2, char *res) {
             mx_printint(r);
             mx_printchar('\n');
         }
-    } else if (*operation== '-') {
+    } else if (*operation == '-') {
         if (o1 - o2 == r) {
             mx_printint(o1);
             mx_printstr(" - ");
@@ -78,7 +95,7 @@ void check(char *operation, char * op1, char *op2, char *res) {
             mx_printint(r);
             mx_printchar('\n');
         }
-    } else if (*operation== '*') {
+    } else if (*operation == '*') {
         if (o1 * o2 == r) {
             mx_printint(o1);
             mx_printstr(" * ");
@@ -88,7 +105,7 @@ void check(char *operation, char * op1, char *op2, char *res) {
             mx_printchar('\n');
         }
     }
-    if (*operation== '/') {
+    if (*operation == '/') {
         if (o1 / o2 == r) {
             mx_printint(o1);
             mx_printstr(" / ");
@@ -99,7 +116,7 @@ void check(char *operation, char * op1, char *op2, char *res) {
         }
     }
 }
-void process(char *operation, char * op1, char *op2, char *res) {
+void process(char *operation, char *op1, char *op2, char *res) {
     int no_questions = 1;
     if (operation[0] == '?') {
         no_questions = 0;
@@ -152,7 +169,6 @@ void process(char *operation, char * op1, char *op2, char *res) {
         }
     }
     if (no_questions) {
-
         check(operation, op1, op2, res);
     }
 }
