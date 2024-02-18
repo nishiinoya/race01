1e690d87cdeabae6747f8a87a13c1b31505db560
nishiinoya <omalanchuk.wrk@gmail.com>
2024-02-18 11:42:08 +0100

    final with checking

--- src/main.c
+++ src/main.c
@@ -9,6 +9,14 @@ int main(int argc, char *argv[]) {
     char *res = format(argv[4]);
 
     process(operation, op1, op2, res);
-
+    
+    free(op1);
+    op1 = NULL;
+    free(operation);
+    operation = NULL;
+    free(op2);
+    op2 = NULL;
+    free(res);
+    res = NULL;
     return 0;
 }
